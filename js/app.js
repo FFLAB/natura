0adef9813245537eb5826c8768bb49ef31841ca9
web1a-thedisciples <FFLAB@users.noreply.github.com>
2024-03-30 14:40:32 +0100

    inprogress

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function toggleFunction() {
+    document.getElementById("action").classList.toggle("add");
+    document.getElementById("button").classList.toggle("change_color");
+     // ALTERNATIVE:
+     //  var element = document.getElementById("action");
+    //   element.classList.add("add_margin");
+    } 
